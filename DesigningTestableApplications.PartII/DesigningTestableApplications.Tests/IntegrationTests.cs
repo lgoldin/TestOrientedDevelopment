bcf754dccc054680066f7bf0d618c876d1249715
lgoldin <leandrogoldin5@gmail.com>
2015-07-21 11:16:30 -0300

    solucion part2 con entity framework. no compila

---
+++ DesigningTestableApplications.PartII/DesigningTestableApplications.Tests/IntegrationTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DesigningTestableApplications.Tests
+{
+    [TestClass]
+    public class IntegrationTests
+    {
+        [TestInitialize]
+        public void Setup()
+        {       
+
+        }
+
+    }
+}
