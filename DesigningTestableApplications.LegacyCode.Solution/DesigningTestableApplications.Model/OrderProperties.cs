c673c1d21e1499bf1f29685a743463de658fd085
Christian Smirnoff <christiansmirnoff@gmail.com>
2016-02-16 11:09:06 -0300

    Refactor Solution + PPT Part III

--- DesigningTestableApplications.LegacyCode.Solution/DesigningTestableApplications.Model/OrderProperties.cs
+++ DesigningTestableApplications.LegacyCode.Solution/DesigningTestableApplications.Model/OrderProperties.cs
@@ -6,6 +6,14 @@ namespace DesigningTestableApplications.Model
 {
     public partial class Order
     {
+        private readonly List<PointsMultiplier> pointsMultipliers = new List<PointsMultiplier>
+                {
+                    new PointsMultiplier { LowerLimit = 1M, UpperLimit = 4999M, Multiplier =  1 },
+                    new PointsMultiplier { LowerLimit = 5000M, UpperLimit = 9999M, Multiplier =  2 },
+                    new PointsMultiplier { LowerLimit = 10000M, UpperLimit = 19999M, Multiplier =  3 },
+                    new PointsMultiplier { LowerLimit = 20000M, UpperLimit = decimal.MaxValue, Multiplier =  4 }
+                };
+
         public virtual decimal GetAmount()
         {
             return
@@ -19,15 +27,7 @@ namespace DesigningTestableApplications.Model
 
         public virtual int GetPoints(decimal amount)
         {
-            var pointsMultipliers = new List<PointsMultiplier>
-                {
-                    new PointsMultiplier { LowerLimit = 1M, UpperLimit = 4999M, Multiplier =  1},
-                    new PointsMultiplier { LowerLimit = 5000M, UpperLimit = 9999M, Multiplier =  2},
-                    new PointsMultiplier { LowerLimit = 10000M, UpperLimit = 19999M, Multiplier =  3},
-                    new PointsMultiplier { LowerLimit = 20000M, UpperLimit = decimal.MaxValue, Multiplier =  4}
-                };
-
-            return (int) (pointsMultipliers.First(x => x.LowerLimit <= amount && x.UpperLimit >= amount).Multiplier * amount);
+            return (int) (this.pointsMultipliers.First(x => x.LowerLimit <= amount && x.UpperLimit >= amount).Multiplier * amount);
         }
     }
 }
